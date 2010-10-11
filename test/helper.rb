32d60032edbad45846410e5ac96000d9b87d785c
Daniel Bretoi <daniel@netwalk.org>
2010-10-10 18:14:52 -0700

    Initial commit to mongoid_autofields.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'mongoid_autofields'
+
+class Test::Unit::TestCase
+end
